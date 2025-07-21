31b12d4ed2b78d08f79a2dd91362abe5c3de706c
Jenil Rathod <119324150+Jenil7828@users.noreply.github.com>
2025-07-21 17:14:02 +0530

    Update apps.py

--- port_folio/apps.py
+++ port_folio/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
 
 class PortFolioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'port_folio'
+
+    def ready(self):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
